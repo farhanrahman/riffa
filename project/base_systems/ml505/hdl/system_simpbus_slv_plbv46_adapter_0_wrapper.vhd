a67e73ad01b46b368c7921a212b970ea800c0604
Farhan Rahman <farhanrahman26@gmail.com>
2012-07-04 11:01:30 +0100

    Added basic pcores to ml505

---
+++ project/base_systems/ml505/hdl/system_simpbus_slv_plbv46_adapter_0_wrapper.vhd
@@ -0,0 +1,212 @@
+-------------------------------------------------------------------------------
+-- system_simpbus_slv_plbv46_adapter_0_wrapper.vhd
+-------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+library simpbus_slv_plbv46_adapter_v1_00_a;
+use simpbus_slv_plbv46_adapter_v1_00_a.all;
+
+entity system_simpbus_slv_plbv46_adapter_0_wrapper is
+  port (
+    SPLB_Clk : in std_logic;
+    SPLB_Rst : in std_logic;
+    PLB_ABus : in std_logic_vector(0 to 31);
+    PLB_UABus : in std_logic_vector(0 to 31);
+    PLB_PAValid : in std_logic;
+    PLB_SAValid : in std_logic;
+    PLB_rdPrim : in std_logic;
+    PLB_wrPrim : in std_logic;
+    PLB_masterID : in std_logic_vector(0 to 1);
+    PLB_abort : in std_logic;
+    PLB_busLock : in std_logic;
+    PLB_RNW : in std_logic;
+    PLB_BE : in std_logic_vector(0 to 7);
+    PLB_MSize : in std_logic_vector(0 to 1);
+    PLB_size : in std_logic_vector(0 to 3);
+    PLB_type : in std_logic_vector(0 to 2);
+    PLB_lockErr : in std_logic;
+    PLB_wrDBus : in std_logic_vector(0 to 63);
+    PLB_wrBurst : in std_logic;
+    PLB_rdBurst : in std_logic;
+    PLB_wrPendReq : in std_logic;
+    PLB_rdPendReq : in std_logic;
+    PLB_wrPendPri : in std_logic_vector(0 to 1);
+    PLB_rdPendPri : in std_logic_vector(0 to 1);
+    PLB_reqPri : in std_logic_vector(0 to 1);
+    PLB_TAttribute : in std_logic_vector(0 to 15);
+    Sl_addrAck : out std_logic;
+    Sl_SSize : out std_logic_vector(0 to 1);
+    Sl_wait : out std_logic;
+    Sl_rearbitrate : out std_logic;
+    Sl_wrDAck : out std_logic;
+    Sl_wrComp : out std_logic;
+    Sl_wrBTerm : out std_logic;
+    Sl_rdDBus : out std_logic_vector(0 to 63);
+    Sl_rdWdAddr : out std_logic_vector(0 to 3);
+    Sl_rdDAck : out std_logic;
+    Sl_rdComp : out std_logic;
+    Sl_rdBTerm : out std_logic;
+    Sl_MBusy : out std_logic_vector(0 to 2);
+    Sl_MWrErr : out std_logic_vector(0 to 2);
+    Sl_MRdErr : out std_logic_vector(0 to 2);
+    Sl_MIRQ : out std_logic_vector(0 to 2);
+    SIMPBUS_ADDR : out std_logic_vector(0 to 31);
+    SIMPBUS_WDATA : out std_logic_vector(0 to 31);
+    SIMPBUS_RDATA : in std_logic_vector(0 to 31);
+    SIMPBUS_BE : out std_logic_vector(0 to 3);
+    SIMPBUS_RNW : out std_logic;
+    SIMPBUS_START : out std_logic;
+    SIMPBUS_DONE : in std_logic;
+    SIMPBUS_ERR : in std_logic
+  );
+end system_simpbus_slv_plbv46_adapter_0_wrapper;
+
+architecture STRUCTURE of system_simpbus_slv_plbv46_adapter_0_wrapper is
+
+  component simpbus_slv_plbv46_adapter is
+    generic (
+      C_BASEADDR : std_logic_vector;
+      C_HIGHADDR : std_logic_vector;
+      C_SPLB_AWIDTH : INTEGER;
+      C_SPLB_DWIDTH : INTEGER;
+      C_SPLB_NUM_MASTERS : INTEGER;
+      C_SPLB_MID_WIDTH : INTEGER;
+      C_SPLB_NATIVE_DWIDTH : INTEGER;
+      C_SPLB_P2P : INTEGER;
+      C_SPLB_SUPPORT_BURSTS : INTEGER;
+      C_SPLB_SMALLEST_MASTER : INTEGER;
+      C_SPLB_CLK_PERIOD_PS : INTEGER;
+      C_INCLUDE_DPHASE_TIMER : INTEGER;
+      C_FAMILY : STRING
+    );
+    port (
+      SPLB_Clk : in std_logic;
+      SPLB_Rst : in std_logic;
+      PLB_ABus : in std_logic_vector(0 to 31);
+      PLB_UABus : in std_logic_vector(0 to 31);
+      PLB_PAValid : in std_logic;
+      PLB_SAValid : in std_logic;
+      PLB_rdPrim : in std_logic;
+      PLB_wrPrim : in std_logic;
+      PLB_masterID : in std_logic_vector(0 to (C_SPLB_MID_WIDTH-1));
+      PLB_abort : in std_logic;
+      PLB_busLock : in std_logic;
+      PLB_RNW : in std_logic;
+      PLB_BE : in std_logic_vector(0 to ((C_SPLB_DWIDTH/8)-1));
+      PLB_MSize : in std_logic_vector(0 to 1);
+      PLB_size : in std_logic_vector(0 to 3);
+      PLB_type : in std_logic_vector(0 to 2);
+      PLB_lockErr : in std_logic;
+      PLB_wrDBus : in std_logic_vector(0 to (C_SPLB_DWIDTH-1));
+      PLB_wrBurst : in std_logic;
+      PLB_rdBurst : in std_logic;
+      PLB_wrPendReq : in std_logic;
+      PLB_rdPendReq : in std_logic;
+      PLB_wrPendPri : in std_logic_vector(0 to 1);
+      PLB_rdPendPri : in std_logic_vector(0 to 1);
+      PLB_reqPri : in std_logic_vector(0 to 1);
+      PLB_TAttribute : in std_logic_vector(0 to 15);
+      Sl_addrAck : out std_logic;
+      Sl_SSize : out std_logic_vector(0 to 1);
+      Sl_wait : out std_logic;
+      Sl_rearbitrate : out std_logic;
+      Sl_wrDAck : out std_logic;
+      Sl_wrComp : out std_logic;
+      Sl_wrBTerm : out std_logic;
+      Sl_rdDBus : out std_logic_vector(0 to (C_SPLB_DWIDTH-1));
+      Sl_rdWdAddr : out std_logic_vector(0 to 3);
+      Sl_rdDAck : out std_logic;
+      Sl_rdComp : out std_logic;
+      Sl_rdBTerm : out std_logic;
+      Sl_MBusy : out std_logic_vector(0 to (C_SPLB_NUM_MASTERS-1));
+      Sl_MWrErr : out std_logic_vector(0 to (C_SPLB_NUM_MASTERS-1));
+      Sl_MRdErr : out std_logic_vector(0 to (C_SPLB_NUM_MASTERS-1));
+      Sl_MIRQ : out std_logic_vector(0 to (C_SPLB_NUM_MASTERS-1));
+      SIMPBUS_ADDR : out std_logic_vector(0 to (C_SPLB_AWIDTH-1));
+      SIMPBUS_WDATA : out std_logic_vector(0 to (C_SPLB_NATIVE_DWIDTH-1));
+      SIMPBUS_RDATA : in std_logic_vector(0 to (C_SPLB_NATIVE_DWIDTH-1));
+      SIMPBUS_BE : out std_logic_vector(0 to (C_SPLB_NATIVE_DWIDTH/8-1));
+      SIMPBUS_RNW : out std_logic;
+      SIMPBUS_START : out std_logic;
+      SIMPBUS_DONE : in std_logic;
+      SIMPBUS_ERR : in std_logic
+    );
+  end component;
+
+begin
+
+  simpbus_slv_plbv46_adapter_0 : simpbus_slv_plbv46_adapter
+    generic map (
+      C_BASEADDR => X"80000000",
+      C_HIGHADDR => X"80001FFF",
+      C_SPLB_AWIDTH => 32,
+      C_SPLB_DWIDTH => 64,
+      C_SPLB_NUM_MASTERS => 3,
+      C_SPLB_MID_WIDTH => 2,
+      C_SPLB_NATIVE_DWIDTH => 32,
+      C_SPLB_P2P => 0,
+      C_SPLB_SUPPORT_BURSTS => 0,
+      C_SPLB_SMALLEST_MASTER => 32,
+      C_SPLB_CLK_PERIOD_PS => 8000,
+      C_INCLUDE_DPHASE_TIMER => 1,
+      C_FAMILY => "virtex5"
+    )
+    port map (
+      SPLB_Clk => SPLB_Clk,
+      SPLB_Rst => SPLB_Rst,
+      PLB_ABus => PLB_ABus,
+      PLB_UABus => PLB_UABus,
+      PLB_PAValid => PLB_PAValid,
+      PLB_SAValid => PLB_SAValid,
+      PLB_rdPrim => PLB_rdPrim,
+      PLB_wrPrim => PLB_wrPrim,
+      PLB_masterID => PLB_masterID,
+      PLB_abort => PLB_abort,
+      PLB_busLock => PLB_busLock,
+      PLB_RNW => PLB_RNW,
+      PLB_BE => PLB_BE,
+      PLB_MSize => PLB_MSize,
+      PLB_size => PLB_size,
+      PLB_type => PLB_type,
+      PLB_lockErr => PLB_lockErr,
+      PLB_wrDBus => PLB_wrDBus,
+      PLB_wrBurst => PLB_wrBurst,
+      PLB_rdBurst => PLB_rdBurst,
+      PLB_wrPendReq => PLB_wrPendReq,
+      PLB_rdPendReq => PLB_rdPendReq,
+      PLB_wrPendPri => PLB_wrPendPri,
+      PLB_rdPendPri => PLB_rdPendPri,
+      PLB_reqPri => PLB_reqPri,
+      PLB_TAttribute => PLB_TAttribute,
+      Sl_addrAck => Sl_addrAck,
+      Sl_SSize => Sl_SSize,
+      Sl_wait => Sl_wait,
+      Sl_rearbitrate => Sl_rearbitrate,
+      Sl_wrDAck => Sl_wrDAck,
+      Sl_wrComp => Sl_wrComp,
+      Sl_wrBTerm => Sl_wrBTerm,
+      Sl_rdDBus => Sl_rdDBus,
+      Sl_rdWdAddr => Sl_rdWdAddr,
+      Sl_rdDAck => Sl_rdDAck,
+      Sl_rdComp => Sl_rdComp,
+      Sl_rdBTerm => Sl_rdBTerm,
+      Sl_MBusy => Sl_MBusy,
+      Sl_MWrErr => Sl_MWrErr,
+      Sl_MRdErr => Sl_MRdErr,
+      Sl_MIRQ => Sl_MIRQ,
+      SIMPBUS_ADDR => SIMPBUS_ADDR,
+      SIMPBUS_WDATA => SIMPBUS_WDATA,
+      SIMPBUS_RDATA => SIMPBUS_RDATA,
+      SIMPBUS_BE => SIMPBUS_BE,
+      SIMPBUS_RNW => SIMPBUS_RNW,
+      SIMPBUS_START => SIMPBUS_START,
+      SIMPBUS_DONE => SIMPBUS_DONE,
+      SIMPBUS_ERR => SIMPBUS_ERR
+    );
+
+end architecture STRUCTURE;
+
---
+++ project/base_systems/ml505/hdl/system_stub.vhd
@@ -0,0 +1,56 @@
+-------------------------------------------------------------------------------
+-- system_stub.vhd
+-------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+entity system_stub is
+  port (
+    fpga_0_PCIe_Bridge_RXN_pin : in std_logic;
+    fpga_0_PCIe_Bridge_RXP_pin : in std_logic;
+    fpga_0_PCIe_Bridge_TXN_pin : out std_logic;
+    fpga_0_PCIe_Bridge_TXP_pin : out std_logic;
+    fpga_0_clk_1_sys_clk_pin : in std_logic;
+    fpga_0_rst_1_sys_rst_pin : in std_logic;
+    fpga_0_PCIe_Diff_Clk_IBUF_DS_P_pin : in std_logic;
+    fpga_0_PCIe_Diff_Clk_IBUF_DS_N_pin : in std_logic
+  );
+end system_stub;
+
+architecture STRUCTURE of system_stub is
+
+  component system is
+    port (
+      fpga_0_PCIe_Bridge_RXN_pin : in std_logic;
+      fpga_0_PCIe_Bridge_RXP_pin : in std_logic;
+      fpga_0_PCIe_Bridge_TXN_pin : out std_logic;
+      fpga_0_PCIe_Bridge_TXP_pin : out std_logic;
+      fpga_0_clk_1_sys_clk_pin : in std_logic;
+      fpga_0_rst_1_sys_rst_pin : in std_logic;
+      fpga_0_PCIe_Diff_Clk_IBUF_DS_P_pin : in std_logic;
+      fpga_0_PCIe_Diff_Clk_IBUF_DS_N_pin : in std_logic
+    );
+  end component;
+
+  attribute BOX_TYPE : STRING;
+  attribute BOX_TYPE of system : component is "user_black_box";
+
+begin
+
+  system_i : system
+    port map (
+      fpga_0_PCIe_Bridge_RXN_pin => fpga_0_PCIe_Bridge_RXN_pin,
+      fpga_0_PCIe_Bridge_RXP_pin => fpga_0_PCIe_Bridge_RXP_pin,
+      fpga_0_PCIe_Bridge_TXN_pin => fpga_0_PCIe_Bridge_TXN_pin,
+      fpga_0_PCIe_Bridge_TXP_pin => fpga_0_PCIe_Bridge_TXP_pin,
+      fpga_0_clk_1_sys_clk_pin => fpga_0_clk_1_sys_clk_pin,
+      fpga_0_rst_1_sys_rst_pin => fpga_0_rst_1_sys_rst_pin,
+      fpga_0_PCIe_Diff_Clk_IBUF_DS_P_pin => fpga_0_PCIe_Diff_Clk_IBUF_DS_P_pin,
+      fpga_0_PCIe_Diff_Clk_IBUF_DS_N_pin => fpga_0_PCIe_Diff_Clk_IBUF_DS_N_pin
+    );
+
+end architecture STRUCTURE;
+
